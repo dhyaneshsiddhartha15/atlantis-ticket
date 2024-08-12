90355597729f5fc6529f25869781b3a2abb95d59
dhyaneshsiddhartha15 <siddharthadhyanesh7@gmail.com>
2024-08-12 21:56:59 +0530

    CORS ERROR

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: "50mb" }));
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://lucent-baklava-0dc47d.netlify.app",
+  "hhttps://atlantis-ticket.vercel.app",
 ];
 
 app.use(
